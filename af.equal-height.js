5e8cb22048d21ec80eb2cfe0cf3348c01df7d6de
Vasilij Belosludcev <bupy765@gmail.com>
2015-02-15 13:37:49 +0500

    Update af.equal-height.js

--- af.equal-height.js
+++ af.equal-height.js
@@ -1,6 +1,9 @@
-// Artfinder Equal Height Children plugin
-//      https://github.com/artfinder/jquery-equal-height
-//      version 1.0
+/** 
+ * Artfinder Equal Height Children plugin
+ *
+ * https://github.com/bupy7/jquery-equal-height
+ * version 1.0
+ */
 
 
 (function($) {
@@ -15,15 +18,15 @@
             $children.each( function() {
                 var $this = $(this);
                 
-                $this.css('min-height', ''); 
-                var h = $this.height();
+                $this.css('height', ''); 
+                var h = $this.outerHeight();
                 if ( h > tallest ) {
                     tallest = h;
                 }
             });
             
             $children.each( function() {
-                $(this).css('min-height', tallest + 'px'); 
+                $(this).css('height', tallest + 'px'); 
             });
         };
         
@@ -32,18 +35,18 @@
         });
     };
 	
-	$.fn.make_children_disequal_height = function () {
-		var make_disequal_height = function (element) {
-			var $within     = $(element),
-				selector    = $within.attr('data-equal-height'),
-				$children   = $(selector, $within);
-				$children.each( function() {function () {
-					$(this).css('min-height', '');
-				}};
-		};
-		return this.each( function () {
-			make_disequal_height(this);
-		});
-	};
+    $.fn.make_children_disequal_height = function () {
+        var make_disequal_height = function (element) {
+            var $within     = $(element),
+            selector    = $within.attr('data-equal-height'),
+            $children   = $(selector, $within);
+            $children.each(function() {
+                $(this).css('height', '');
+            });
+        };
+        return this.each( function () {
+                make_disequal_height(this);
+        });
+    };
     
 })( jQuery );
